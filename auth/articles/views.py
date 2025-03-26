49dafb6947cec6a212bba2a1f02ee283a39c007e
yoojin <euvb918@gmail.com>
2025-03-26 17:19:57 +0900

    250326

---
+++ auth/articles/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from .forms import ArticleForm
+from .models import Article
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+
+    context = {
+        'articles': articles,
+    }
+
+    return render(request, 'index.html', context)
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:index')
+    else:
+        form = ArticleForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'create.html', context)
+
+def detail(request, id):
+    article = Article.objects.get(id=id)
+
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'detail.html', context)
+
+def update(request, id):
+    article = Article.objects.get(id=id)
+    
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', id=id)
+    else:
+        form = ArticleForm(instance=article)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'update.html', context)
+
+def delete(request, id):
+    article = Article.objects.get(id=id)
+    article.delete()
+
+    return redirect('articles:index')
