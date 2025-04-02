c146532162d0bbe2757bc114da14711569390eb9
yoojin <euvb918@gmail.com>
2025-04-02 16:08:35 +0900

    250402 like

--- insta/posts/urls.py
+++ insta/posts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
     path('<int:post_id>/comments/create/', views.comment_create, name='comment_create'),
-    path('<int:post_id>/like', views.like, name='like'),
+    path('<int:post_id>/like/', views.like, name='like'),
     path('feed/', views.feed, name='feed'),
+    path('<int:id>/like-async/', views.like_async, name='like_async'),
 ]
